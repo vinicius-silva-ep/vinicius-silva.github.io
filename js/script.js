c0dd64545d056c46921ff392751b820acb75021d
viniscera <vinicius.silva.ep@outlook.com>
2024-03-16 20:32:31 -0300

    redução do texto inicial e atualizando o eng

--- js/script.js
+++ js/script.js
@@ -39,15 +39,15 @@ $(document).ready(function() {
                     break;
             }
         });
-        $(".sobre-mim p.mb-5").text("Hello! I'm Vinícius, production engineer, graduated from the Federal University of Western Bahia. Since 2020, I've been making my way in the world of data. My journey involves combining my solid background in production engineering and my deep-rooted passion for technology. Over the years, I focused my efforts in the area of Data Science, exploring different aspects, from data analysis to the world of Business Intelligence. My main focus lies in Data Engineering. When I'm not immersed in data challenges, I'm an instrumental musician in my spare time. I believe that combining the logic of engineering with the creativity of music is the key to a unique and innovative approach. My goal is to continue improving my skills, exploring new frontiers of data science and contributing to smart and impactful solutions. This is my world, where the passion for technology and the incessant search for knowledge shape my professional and personal journey.");
+        $(".sobre-mim p.mb-5").text("Hello! I'm Vinícius, production engineer, graduated from the Federal University of Western Bahia. Since 2020, I've been making my way in the world of data. My journey involves combining my solid background in production engineering and my deep-rooted passion for technology. Over the years, I focused my efforts in the area of Data Science, exploring different aspects, from data analysis to the world of Business Intelligence. My main focus lies in Data Engineering.");
         // $("#meus_projetos h2.mb-5").text('My Projects');
         // $("#meus_projetos h3.mb-3").text('Project Management');
         // $("#meus_projetos p").text('With the aim of organizing my personal study activities, I created this Power BI dashboard with phases, project, activity descriptions, deadlines, and status. This way, I can know what is pending, what is already finished, and everything I have not started yet.');
         $(".btn-custom").each(function() {
             // Verifica se o texto do botão é o texto original em inglês
-            if ($(this).text() === 'Clique aqui para visualizar o painel em tela inteira') {
+            if ($(this).text() === 'Ver em tela inteira') {
                 // Se for, traduz para inglês
-                $(this).text('Click here to view the dashboard in full screen');
+                $(this).text('View in full screen');
             }
         });
         $(".text-muted").each(function() {
@@ -57,5 +57,10 @@ $(document).ready(function() {
                 $(this).text('(All data is public, including dashboards)');
             }
         });
+        // Traduz o conteúdo da seção "Meus Projetos"
+        $("#meus_projetos h2.mb-5").text('My Projects');
+        $("#meus_projetos h3.mb-3").text('Project Management');
+        $("#meus_projetos p").text('With the aim of organizing my personal study activities, I created this Power BI dashboard with phases, project, activity descriptions, deadlines, and status. This way, I can know what is pending, what is already finished, and everything I have not started yet.');
+
     }
 });
